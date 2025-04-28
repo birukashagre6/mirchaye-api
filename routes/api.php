8b05da73faebe7f1cc125da3f189c182033c537f
birukashagre6 <mandave693@gmail.com>
2025-04-28 16:27:34 +0300

    add controller for handling NEBE admin authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\PoliticalPartyAuthController;
+use App\Http\Controllers\Api\Auth\NEBEAuthController;
 
 Route::prefix('party')->group(function () {
     Route::post('/register', [PoliticalPartyAuthController::class, 'register']);
@@ -10,5 +11,10 @@ Route::prefix('party')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [PoliticalPartyAuthController::class, 'logout']);
         Route::get('/me', [PoliticalPartyAuthController::class, 'me']);
+    });
+
+    Route::prefix('nebe')->group(function () {
+        // NEBE Admin Login
+        Route::post('/login', [NEBEAuthController::class, 'login']);
     });
 });
